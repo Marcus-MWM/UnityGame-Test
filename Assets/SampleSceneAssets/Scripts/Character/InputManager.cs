c0d699c27480fb3981f35a5b498c55a42719d2a7
Marcus-MWM <marcusmc10101@gmail.com>
2023-02-27 14:59:48 -0800

    First Commit

---
+++ Assets/SampleSceneAssets/Scripts/Character/InputManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    // private PlayerInput playerInput;
+    // private PlayerInput.CharacterActions character;
+    // // Start is called before the first frame update
+    // void Awake()
+    // {
+    //     playerInput = new PlayerInput();
+    //     character = playerInput.CharacterActions;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    // private void OnEnable(){
+    //     character.Enable();
+    // }
+
+    // private void OnDisable(){
+    //     character.Disable();
+    // }
+}
